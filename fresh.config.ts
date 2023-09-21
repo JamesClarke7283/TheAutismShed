d2fd6bcde8399080d2ef69b58c3a97898ba50575
James David Clarke <james@jamesdavidclarke.com>
2023-09-21 08:11:39 +0100

    Added SEO plugin to generate sitemaps for site

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "$fresh/server.ts";
-import twindPlugin from "$fresh/plugins/twind.ts"
+import twindPlugin from "$fresh/plugins/twind.ts";
+import { freshSEOPlugin } from "https://deno.land/x/fresh_seo@1.0.1/mod.ts";
+import manifest from "./fresh.gen.ts";
 import twindConfig from "./twind.config.ts";
 export default defineConfig({
-  plugins: [twindPlugin(twindConfig)]
+  plugins: [
+    twindPlugin(twindConfig),
+    freshSEOPlugin(manifest, {
+      include: ["/staff/*"],
+    }),
+  ],
 });
